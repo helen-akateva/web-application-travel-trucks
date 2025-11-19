da7e92556a54c8361769ab73e26a5823e6d71bbf
helen-akateva <akareva17@gmail.com>
2025-11-19 16:53:00 +0200

    implement home page with hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,22 @@
+import Link from "next/link";
+import css from "./Home.module.css";
 
+export default function Home() {
+  return (
+    <main className={css.main}>
+      <section className={css.hero}>
+        <div  className={css.container}>
+        <h1 className={css.title}>Campers of your dreams</h1>
+
+        <p className={css.description}>
+          You can find everything you want in our catalog
+        </p>
+        <Link href="/catalog" className={css.button}>
+          View Now
+        </Link>
+      </div>
+      </section>
+      
+    </main>
+  );
+}
