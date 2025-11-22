12c76bd9604c0688594a30acb5d9eaa45724d634
helen-akateva <akareva17@gmail.com>
2025-11-22 09:23:00 +0200

    install toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
 import Header from "@/components/Header/Header";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -27,6 +28,23 @@ export default function RootLayout({
         <TanStackProvider>
           <Header/>
           <main>{children}</main>
+             <Toaster
+            position="top-right"
+            toastOptions={{
+              duration: 4000,
+              style: {
+                background: '#fff',
+                color: '#101828',
+                border: '1px solid #DADDE1',
+              },
+              success: {
+                iconTheme: {
+                  primary: '#E44848',
+                  secondary: '#fff',
+                },
+              },
+            }}
+          />
         </TanStackProvider>
       </body>
     </html>
